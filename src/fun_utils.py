a421b14b9152023e6bcca1cbe8d01d5ce94419d4
lasercata <67599917+lasercata@users.noreply.github.com>
2025-11-05 16:28:02 +0100

    Implementing `split_data` (#3)
    Tests are passing

--- src/fun_utils.py
+++ src/fun_utils.py
@@ -27,8 +27,36 @@ def load_data(filename):
 
 
 def split_data(x, y, tr_fraction=0.5):
+    """Split the data x,y into two random subsets.
+    
+    input:
+        x: set of images
+        y: labels
+        fract_tr: float, percentage of samples to put in the training set.
+            If necessary, number of samples in the training set is rounded to
+            the lowest integer number.
+    
+    output:
+        Xtr: set of images (numpy array, training set)
+        Xts: set of images (numpy array, test set)
+        ytr: labels (numpy array, training set)
+        yts: labels (numpy array, test set)
+    
     """
-    Split the data x, y into two random subsets
 
-    """
-    pass
+    num_samples = y.size
+    n_tr = int(num_samples * tr_fraction)
+
+    idx = np.array(range(0, num_samples))
+    np.random.shuffle(idx)  # shuffle the elements of idx
+
+    tr_idx = idx[0:n_tr]
+    ts_idx = idx[n_tr:]
+
+    Xtr = x[tr_idx, :]
+    ytr = y[tr_idx]
+
+    Xts = x[ts_idx, :]
+    yts = y[ts_idx]
+
+    return Xtr, ytr, Xts, yts
